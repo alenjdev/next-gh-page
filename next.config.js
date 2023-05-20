c35811dfe1229a007b34b76aee365ac513c53e1d
Alen <alen@formant.io>
2023-05-19 22:36:43 -0500

    add image support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
-  };
-   
-  module.exports = nextConfig;
+  output: "export",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
+};
+
+module.exports = nextConfig;
